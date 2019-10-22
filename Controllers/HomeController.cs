948ffc8efa87d97467835021a9a28bfa619b58dd
Annah <annatardream@gmail.com>
2019-10-22 19:35:33 +0300

    design changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ArtSkills.Data;
 using Microsoft.AspNetCore.Mvc;
 using ArtSkills.Models;
 
@@ -10,12 +11,17 @@ namespace ArtSkills.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext applicationDbContext { get; }
+
+        public HomeController(ApplicationDbContext context)
+        {
+            applicationDbContext = context;
+        }
         public IActionResult Index()
         {
-            Wall homeWall = new Wall();
-            if (homeWall.Arts.Count == 0)
+            if (applicationDbContext.Arts.ToList().Count == 0)
                 return RedirectToAction("ErrorWhileLoading", "Home");
-            else return View();
+            else return View(applicationDbContext.Arts.ToList());
         }
 
         public IActionResult Privacy()
@@ -31,7 +37,7 @@ namespace ArtSkills.Controllers
 
         public IActionResult ErrorWhileLoading()
         {
-            AppMessage appMessage = new AppMessage("/Resources/sorry.gif", "Cannot load any posts");
+            AppMessage appMessage = new AppMessage("~/Resources/sorry.gif", "Cannot load any posts");
             return View();
         }
     }
