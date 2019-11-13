9d8be73a8a4faed4cbd5918439ab3c988642d4e3
Annah <annatardream@gmail.com>
2019-11-13 13:53:13 +0300

    added view components.

---
+++ Components/LoginViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ArtSkills.Models;
+using ArtSkills.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Task = System.Threading.Tasks.Task;
+
+namespace ArtSkills.Components
+{
+    public class LoginViewComponent : ViewComponent
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LoginViewComponent(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                return View("LoggedIn", user);
+            }
+            else return View("NotLoggedIn");
+        }
+    }
+}
---
+++ Components/UserLikeViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ArtSkills.Models;
+using ArtSkills.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Task = System.Threading.Tasks.Task;
+
+namespace ArtSkills.Components
+{
+    public class UserLikeViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserLikeViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public async Task<IViewComponentResult> InvokeAsync(Art art)
+        {
+            ApplicationUser user = await GetCurrentUserAsync();
+
+            if (user == null)
+            {
+                return View("DisabledLikeButton", art);
+            }
+            else
+            {
+                if (user.Id == art.ApplicationUser.Id)
+                    return View("DisabledLikeButton", art);
+                else return View("LikableButton", art);
+            }
+        }
+    }
+}
